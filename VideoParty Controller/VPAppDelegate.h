c3ab7af6a53a534e559a463947fb0b38d8573f29
cochrane <torsten@ferroequinologist.de>
2012-08-12 00:33:35 +0200

    Added the files for the controller (iOS) app.
    For some reason, they weren't present originally. I thought I had added them, but it seems that I didn't. Sorry about that. Right now, they are just empty, on default values.

---
+++ VideoParty Controller/VPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VPAppDelegate.h
+//  VideoParty Controller
+//
+//  Created by Torsten Kammer on 10.08.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoParty Controller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoParty Controller
+//
+//  Created by Torsten Kammer on 10.08.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([VPAppDelegate class]));
+	}
+}
